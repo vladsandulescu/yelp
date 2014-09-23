404a1ca9b669e5cc25f0428143fc731468c48eea
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-09-23 20:49:04 +0200

    change settings including api key

--- settings/settings.py
+++ settings/settings.py
@@ -6,10 +6,11 @@ class Settings:
         pass
 
     IMPORTIO_USER_ID = "98e321b1-614c-4b53-bd78-212804a036d1"
-    IMPORTIO_API_KEY = "ucOzf65gTH3LY9LOAuP0Gbr0KIrfxlTcHr7Q7v94VrViij95fK8GIjEYj7qz+q644GMmpEkGPg5FIunztbkm8g=="
+    IMPORTIO_API_KEY = "FAkA7z0C8iZbjynO/6S8IhBhLttMI0HYEcN2/VYXjX+Ae1K0L945UUkjrEtww+dPzPxwroipuKfb1f5fPe514Q=="
     IMPORTIO_HOST = "https://query.import.io"
     IMPORTIO_CRAWLER_ACTIVE = ["5344191b-ac10-4f28-9d11-ee2ca522727c"]
     IMPORTIO_CRAWLER_FILTERED = ["3988818d-7d21-4552-93d7-e8b40bd28bee"]
+    IMPORTIO_CRAWLER_FRIENDS = ["a63ca815-4aac-44d6-bb76-81f546423f00"]
 
     YELP_CONSUMER_KEY = 'E1TkOV8sFtW4u7-Db0iLIg'
     YELP_CONSUMER_SECRET = 'B3re7Kn4-L0kPuXZlfqueTdj19c'
@@ -17,4 +18,4 @@ class Settings:
     YELP_TOKEN_SECRET = 'Js_vCDOCts-MQB8-bZ2WRTkpfYw'
 
     CONNECTION_STRING = "mongodb://localhost:27030/"
-    DATABASE_NAME = "Yelp"
+    DATABASE_NAME = "Yelp_with_Users"
