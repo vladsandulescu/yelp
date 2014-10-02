0cd1c0833cac17eb0ff91b02a5754c78a0b9a992
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-10-02 20:06:16 +0200

    increase max retries

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def run(collection):
     filtered_urls_file = 'data/filtered_urls_' + collection + '.json'
 
     retries = 0
-    max_retries = 3
+    max_retries = 10
     result = False
 
     while result is False and retries < max_retries:
@@ -20,7 +20,7 @@ def run(collection):
             retries += 1
             print "Unexpected error:", sys.exc_info()[0]
             print "Will retry to run"
-            if retries == 3:
+            if retries == max_retries:
                 print "Unexpected error:", sys.exc_info()[0]
                 print "Max retries number was reached. Will stop."
                 raise
