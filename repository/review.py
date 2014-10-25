2463a3890c53737c40c237bdd4ea6438e829e75b
Vlad Sandulescu <vlad.sandulescu@gmail.com>
2014-10-25 09:49:12 +0200

    handle rating and date in constructor

--- repository/review.py
+++ repository/review.py
@@ -1,3 +1,7 @@
+import calendar
+import re
+import datetime
+
 __author__ = 'Vlad'
 
 
@@ -5,9 +9,9 @@ class Review:
     def __init__(self, friends, reviews, date, text, rating, user):
         self.friends = friends
         self.reviews = reviews
-        self.date = date
+        self.date = self.getDate(date)
         self.text = text
-        self.rating = rating
+        self.rating = self.getRating(rating)
         self.user = user
 
     def __cmp__(self, other):
@@ -20,10 +24,23 @@ class Review:
                 "reviews": self.reviews,
                 "date": self.date,
                 "text": self.text,
-                "rating": self.getRating(self.rating),
+                "rating": self.rating,
                 "user": self.user.encode()
             }
 
     @staticmethod
     def getRating(html):
         return html[10:13]
+
+    def getDate(self, date):
+        if isinstance(date, list):
+            match = re.search(r'(\d+/\d+/\d+)', date[0])
+            extracted_date = match.group()
+        else:
+            extracted_date = date
+        try:
+            pretty_date = calendar.timegm(datetime.datetime.strptime(extracted_date, "%m/%d/%Y").timetuple())
+        except:
+            pretty_date = 0
+
+        return pretty_date
